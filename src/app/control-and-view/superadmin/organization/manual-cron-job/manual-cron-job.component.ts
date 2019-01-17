4ba41e53495e09bd5357a6415afd45bb637fca13
rodneyThomas <rodney.troodon@outlook.com>
2019-01-17 14:18:36 +0530

    styling

---
+++ src/app/control-and-view/superadmin/organization/manual-cron-job/manual-cron-job.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-cron-job',
+  templateUrl: './manual-cron-job.component.html',
+  styleUrls: ['./manual-cron-job.component.scss']
+})
+export class ManualCronJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/control-and-view/superadmin/organization/organization.module.ts
+++ src/app/control-and-view/superadmin/organization/organization.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CreateOrganizationComponent } from './create-organization/create-organization.component';
 import { ViewOrganizationComponent } from './view-organization/view-organization.component';
 import { EditOrganizationComponent } from './edit-organization/edit-organization.component';
+import { ManualCronJobComponent } from './manual-cron-job/manual-cron-job.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CreateOrganizationComponent, ViewOrganizationComponent, EditOrganizationComponent]
+  declarations: [CreateOrganizationComponent, ViewOrganizationComponent, EditOrganizationComponent, ManualCronJobComponent]
 })
 export class OrganizationModule { }
